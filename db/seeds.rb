c8dfaa2219cbdb4fabeeacfd05e6dc0c31e7017f
Homies886 <Niko876@hotmail.com>
2020-06-16 14:45:08 -0400

    Added Genre model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+sports = Genre.create(name: "Sports")
+rpg = Genre.create(name: "RPG")
+strategy = Genre.create(name: "Strategy")
